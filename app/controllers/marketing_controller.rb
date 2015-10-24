7d46946af4a119316df866ccaccd9eeb3c9884aa
Tristan Rubadeau <tristan@rubadeau.com>
2015-10-23 22:49:49 -0600

    Added Clearance model and views

---
+++ app/controllers/marketing_controller.rb
@@ -0,0 +1,9 @@
+class MarketingController < ApplicationController
+
+  def index
+
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,14 @@
+
 Rails.application.routes.draw do
+  constraints Clearance::Constraints::SignedIn.new { |user| user.admin? } do
+    root to: 'admin/dashboards#show', as: :admin_root
+  end
+
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: 'dashboards#show', as: :signed_in_root
+  end
+
+  constraints Clearance::Constraints::SignedOut.new do
+    root to: 'marketing#index'
+  end
 end
